ca44a5cb8c79f967a7fabb4f7baed806230acff8
yanxs <541139655@qq.com>
2018-01-19 16:46:27 +0800

    广告添加TimestampBehavior对时间处理

--- common/models/Ads.php
+++ common/models/Ads.php
@@ -3,6 +3,7 @@
 namespace common\models;
 
 use Yii;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "{{%ads}}".
@@ -26,6 +27,12 @@ class Ads extends \yii\db\ActiveRecord
     public static function tableName()
     {
         return '{{%ads}}';
+    }
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
     }
 
     /**
